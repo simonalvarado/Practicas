f30fe2a5d138f4f4a6003cdc0b2b920182b3a6f9
simonalvarado <simonjesusalvarado@gmail.com>
2021-07-13 16:48:54 -0300

    Commit after time

---
+++ VUE/First Example/moz-todo-vue/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <h1>To-Do List</h1>
+    <ul>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import ToDoItem from './components/ToDoItem.vue';
+  import uniqueId from 'lodash.uniqueid'; //para agregar un valor unico a cada elemento de nuestra lista
+
+  //component object
+  export default {
+    name: 'App',
+    components: {
+      ToDoItem
+    },
+    data() {
+      return {
+        ToDoItems: [
+          { id:uniqueId('todo-'), label: 'Learn Vue', done: false },
+          { id:uniqueId('todo-'), label: 'Create a vue project with the CLI', done: true },
+          { id:uniqueId('todo-'), label: 'Have fun', done: true },
+          { id:uniqueId('todo-'), label: 'Create a to-do list', done: false }
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ VUE/First Example/moz-todo-vue/src/components/ToDoItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <input type="checkbox" :id="id" :checked="isDone" />
+        <label :for="id">{{label}}</label>
+    </div>
+</template>
+
+<script>
+import uniqueId from 'lodash.uniqueid';
+
+//object
+export default {
+    props: {
+        label: {required: true, type: String},
+        done: {default: false, type: Boolean},
+        id: {required: true, type: String}
+    },
+    data() {
+        return {
+            isDone: this.done,
+        };
+    }
+};
+</script>
